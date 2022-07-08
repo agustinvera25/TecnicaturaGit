26b51e22c8883bd180de5148a50c1dae7262930a
Ariel Betancud <betancudariel@gmail.com>
2022-07-08 17:13:19 -0300

    Cargando las clases que voy avanzando en python y java

---
+++ Java/Leccion2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -0,0 +1,13 @@
+
+package CicloWhile;
+
+public class EjercicioWhile01 {
+    public static void main(String[] args) {
+        var conteo = 0; // Inferencia de tipos
+        while(conteo < 3){
+            System.out.println("conteo = " + conteo);
+            conteo++; //Vamos aumentando en uno la variable
+        }
+        
+    }
+}
---
+++ Python/Lección4/main.py
@@ -0,0 +1,9 @@
+# lista = Ariel , Liliana, Natalia, Osvaldo
+
+nombres = ['Naty', 'Osvaldo', 'Lily', 'Ariel']
+print(nombres)
+print(nombres[0])
+print(nombres[1])
+print(nombres[3])
+print(nombres[-1])
+print(nombres[-2])
