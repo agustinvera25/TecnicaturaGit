35753f5c1560eeae607820dab189a9e3b4d79da9
Ariel Betancud <betancudariel@gmail.com>
2022-07-19 08:02:31 -0300

    Actualizamos los commit de la leccion 2 js y python

---
+++ Javascript/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,24 @@
+//Creamos un nuevo archivo
+/*
+Empezamos a ver
+los comentario
+en JavaScript
+*/
+//Tipo String
+var nombre = "Ariel";
+console.log(typeof nombre);
+nombre = 7;
+console.log(typeof nombre);
+nombre = 12.3;
+console.log(typeof nombre);
+//Tipo numerico
+var numero = 3000;
+console.log(numero)
+//Tipo Object
+var objeto = {
+    nombre : "Ariel",
+    apellido : "Betancud",
+    telefono : "2614547489"
+}
+
+console.log(objeto);
