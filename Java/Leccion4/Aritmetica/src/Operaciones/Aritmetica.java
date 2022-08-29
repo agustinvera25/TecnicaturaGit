035b3ef6c5aca92fa86e721afb463866c4496cc3
Ariel Betancud <betancudariel@gmail.com>
2022-08-29 15:28:28 -0300

    Commiteamos python Ejercicios del 7 al 11 y Java creación de clases

--- Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -11,4 +11,9 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
     }
+    
+    public int sumarConRetorno(){
+        //int resultado = a + b;
+        return a + b;
+    }
 }
